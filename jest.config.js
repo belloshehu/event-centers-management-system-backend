bbd7a502f11e44fb80b723fb320b623ebf9cbdc9
Bello Shehu <belloshehu1@gmail.com>
2025-03-09 23:30:55 +0100

    Fix action config

--- jest.config.js
+++ jest.config.js
@@ -1,32 +1,4 @@
-// /** @type {import('ts-jest').JestConfigWithTsJest} **/
-// // import tsconfig from "./tsconfig.json";
-// // //ts-ignore
-// //import moduleNameMapper from "tsconfig-paths-jest";
-
-// module.exports = {
-// 	preset: "ts-jest",
-// 	testEnvironment: "node",
-// 	transform: {
-// 		"^.+.tsx?$": ["ts-jest", {}],
-// 	},
-// 	testMatch: ["**/*.test.ts"],
-// 	verbose: true,
-// 	forceExit: true,
-// 	//moduleNameMapper: moduleNameMapper(tsconfig),
-// 	moduleDirectories: ["node_modules", "<rootDir>/src"],
-// 	moduleNameMapper: {
-// 		"@controllers/(.*)": "<rootDir>/src/controllers/$1",
-// 		"@config/(.*)": "<rootDir>/src/config/$1",
-// 		"@database/(.*)": "<rootDir>/src/database/$1",
-// 		"@errors/(.*)": "<rootDir>/src/errors/$1",
-// 		"services/(.*)": "<rootDir>/src/services/$1",
-// 		"@middleware/(.*)": "<rootDir>/src/middleware/$1",
-// 		"@models/(.*)": "<rootDir>/src/models/$1",
-// 		"@routes/(.*)": "<rootDir>/src/routes/$1",
-// 		"@types/(.*)": "<rootDir>/src/types/$1",
-// 		"@util/(.*)": "<rootDir>/src/util/$1",
-// 	},
-// };
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
 
 import { pathsToModuleNameMapper } from "ts-jest";
 import { createRequire } from "module";
